21ddc1901ce0ccb5285db0b1c4b8bb65399a45bc
AlexanderDykov <alexander.dykov@gmail.com>
2015-07-01 22:15:34 +0300

    -Initialize scene for second game

---
+++ Assets/_ZigZag clone/_Scripts/ZigZagController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZigZagController : MonoBehaviour {
+
+    public float speed = 5f;
+    Vector3 direction = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            direction = (direction == Vector3.forward) ? Vector3.left : Vector3.forward;
+            
+        }
+        float temp = speed * Time.deltaTime;
+        transform.Translate(direction * temp);
+	}
+}
